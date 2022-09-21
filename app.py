53424bcf492a2b30da4765f3f6929b72b1fb47fa
hyunjin9603 <84309186+hyunjin9603@users.noreply.github.com>
2022-09-21 16:00:35 +0900

    로그인/회원가입

--- app.py
+++ app.py
@@ -17,44 +17,60 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
-@app.route("/coding", methods=["POST"])
-def coding_post():
-    coding_receive = request.form["coding_give"]
-    meaning_receive = request.form["meaning_give"]
+        return render_template('index.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-    count = db.coding.count_documents({})
-    num = count + 1
 
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up(): #회원가입
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        'num': num,
-        'coding': coding_receive,
-        'meaning': meaning_receive,
-        'done': 0
+        "username": username_receive,                       # 아이디
+        "password": password_hash                           # 비밀번호
     }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
 
-    db.coding.insert_one(doc)
-    return jsonify({'msg':'등록 완료!'})
-
-@app.route("/coding/restore", methods=["POST"])
-def coding_restore():
-    num_receive = request.form["num_give"]
-    db.coding.update_one({'num': int(num_receive)}, {'$set': {'done': 0}})
-    return jsonify({'msg': '북마크 취소!'})
-
-@app.route("/coding/bookmark", methods=["POST"])
-def coding_bookmark():
-    num_receive = request.form["num_give"]
-    db.coding.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
-    return jsonify({'msg': '북마크 완료!'})
-
-@app.route("/coding", methods=["GET"])
-def coding_get():
-    coding_list = list(db.coding.find({},{'_id':False}))
-    return jsonify({'coding':coding_list})
-
-
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=4000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
